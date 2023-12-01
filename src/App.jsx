21fabf7f11128572283e51e7a4bff2bb0189f55d
03ell <s2200570@student.unklab.ac.id>
2023-12-01 15:48:38 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './assets/css/aos.css'
+import './assets/css/bootstrap.css'
+import './assets/css/bootstrap.min.css'
+import './assets/css/main.css'
+import Header from './components/header'
+import Body from './components/body'
+import Footer from './components/footer'
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Body/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
